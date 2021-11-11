aecb9f054083ea76bf24e6152cd99735c583740a
Sam F <sampfergie@gmail.com>
2021-11-11 14:24:09 +0000

    Routing, Login Page and API
    Added routing from one page to another. Created a login form and the start of an express API.
    
    Next steps are to further develop the Express API, generate tests for all endpoints and connect the working API to the login page.

--- dnd-project/src/app/app-routing.module.ts
+++ dnd-project/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MonsterFormComponent } from './monster-form/monster-form.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login-page', pathMatch: 'full' },
+  { path: 'login-page', component: LoginPageComponent },
+  { path: 'monster-form', component: MonsterFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dnd-project/src/app/app.module.ts
+++ dnd-project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MonsterFormComponent } from './monster-form/monster-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MonsterFormComponent
   ],
   imports: [
     BrowserModule,
